99004e875f8eec3f37e593c30d3ed6db7d1b834e
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2022-04-05 16:17:22 -0500

    added title, fave links, and repositioned elements

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ['https://stardewvalleywiki.com/Stardew_Valley_Wiki','https://www.mylibrary.org/']
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
