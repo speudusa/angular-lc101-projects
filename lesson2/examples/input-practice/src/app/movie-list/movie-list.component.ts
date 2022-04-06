49af7d4e02bccd03d21545068722dc25de6bd2bb
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2022-04-06 15:11:50 -0500

    completed chapter examples

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -12,4 +12,11 @@ export class MovieListComponent implements OnInit {
 
    ngOnInit() {
    }
+
+   addMovie (newTitle:string){
+      if(!this.movies.includes(newTitle) && newTitle != '' ){
+         this.movies.push(newTitle);
+      } 
+         
+   }
 }
